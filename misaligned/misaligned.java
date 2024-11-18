10b420aa81e197d91ada0946d7c9d59c295be674
Khushdeep2004 <99407262+Khushdeep2004@users.noreply.github.com>
2024-11-18 10:24:52 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -3,9 +3,9 @@ public class Misaligned {
         String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
         int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+        for(i = 0; i < majorColors.length; i++) {
+            for(j = 0; j < minorColors.length; j++) {
+                System.out.printf("%d | %s | %s \n", i * minorColors.length + j, majorColors[i], minorColors[i]);
             }
         }
         return i * j;
